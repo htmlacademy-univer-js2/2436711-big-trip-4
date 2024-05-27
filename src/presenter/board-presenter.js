d743a84b854b78cf789d54abfef7d357edbbf65e
dradedam <130477584+dradedam@users.noreply.github.com>
2024-05-27 22:43:31 +0500

    5.9. Большие перемены (часть 2)

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,8 +1,9 @@
-import {render} from '../framework/render';
+import {remove, render} from '../framework/render';
 import PointListView from '../view/point-list-view';
 import SortView from '../view/sort-view';
 import EmptyPointView from '../view/empty-point-view';
 import PointPresenter from './point-presenter';
+import {sortDay, sortPrice, sortTime} from '../utils';
 
 export default class BoardPresenter {
   constructor({container, pointsModel }) {
@@ -13,9 +14,16 @@ export default class BoardPresenter {
 
   init() {
     this.points = [...this.pointsModel.getPoints()];
+    this.sortedPoints = [...this.points.sort(sortDay)];
+    this.sortType = 'day';
+
     if (this.points.length) {
+      this.sortComponent = new SortView({
+        onSortTypeChange: this.handleSortTypeChange,
+        sortType: this.sortType
+      });
+
       this.pointsListComponent = new PointListView();
-      this.sortComponent = new SortView();
 
       render(this.sortComponent, this.container);
       render(this.pointsListComponent, this.container);
@@ -28,6 +36,7 @@ export default class BoardPresenter {
 
   handlePointChange = (updatedPoint) => {
     this.points = this.points.map((item) => item.id === updatedPoint.id ? updatedPoint : item);
+    this.sortedPoints = this.sortedPoints.map((item) => item.id === updatedPoint.id ? updatedPoint : item);
     this.pointPresenters.get(updatedPoint.id).init(updatedPoint);
   };
 
@@ -43,4 +52,34 @@ export default class BoardPresenter {
     pointPresenter.init(point);
     this.pointPresenters.set(point.id, pointPresenter);
   }
+
+  handleSortTypeChange = (sortType) => {
+    if (this.sortType === sortType) {
+      return;
+    }
+
+    switch(sortType) {
+      case 'time':
+        this.points.sort(sortTime);
+        break;
+      case 'price':
+        this.points.sort(sortPrice);
+        break;
+      default:
+        this.points = [...this.sortedPoints];
+    }
+    this.sortType = sortType;
+
+    this.pointPresenters.forEach((presenter) => presenter.destroy());
+    this.pointPresenters.clear();
+    remove(this.sortComponent);
+    this.sortComponent = new SortView({
+      onSortTypeChange: this.handleSortTypeChange,
+      sortType: this.sortType
+    });
+    render(this.sortComponent, this.container);
+    render(this.pointsListComponent, this.container);
+
+    this.points.forEach((point) => this.renderPoint(point));
+  };
 }
